9ed07fde215dd2316cfa0cbf46dd7f18346b9afc
Vutisat <vutisat@miamioh.edu>
2015-10-11 18:42:48 -0400

    Started working on the opening moves

--- Domineering/src/Hercules.java
+++ Domineering/src/Hercules.java
@@ -1,10 +1,19 @@
-package game;
+package domineering;
+
+import game.GameMove;
+import game.GamePlayer;
+import game.GameState;
 
 public class Hercules extends GamePlayer{
 
+	private int rows = 0;
+	private int cols = 0;
+	protected DomineeringMove [] openingMoves;
+	
 	public Hercules(String n) 
 	{
 		super(n, "Domineering");
+		createOpeningMoves();
 	}
 
 	/**
@@ -17,7 +26,26 @@ public class Hercules extends GamePlayer{
 	}
 
 	public GameMove getMove(GameState state, String lastMove){
+		/*The following code is for our opening moves
+		 *  Home is horizontal
+		 *	Away is Vertical
+		 */
+		
 		return null;
+		//End of opening moves.
+		
+	}
+	
+	public void createOpeningMoves(){
+		if(side.equals("AWAY")){
+			for(int i = 0; i < rows -1; i++){
+				
+			}
+		}else if(side.equals("HOME")){
+			for(int i = 0; i < cols -1; i++){
+				
+			}
+		}
 	}
 		
 }
